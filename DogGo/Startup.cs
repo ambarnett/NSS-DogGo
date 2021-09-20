6fb131b6180d12f1bd26bba5363abb396c59ea68
ambarnett <andy.barnett09@gmail.com>
2021-09-20 14:54:56 -0500

    added owner.cs

--- DogGo/Startup.cs
+++ DogGo/Startup.cs
@@ -27,7 +27,7 @@ namespace DogGo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddTransient<IWalkerRepository, WalkerRepository>();
+            services.AddTransient<IWalkerRepository, WalkerRepository>(); //This is saying "anytime my program asks for "IWalkerRepository" make "WalkerRepository"
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
